c63245de01cf3dfe6b768cb3d95486952de2d130
Nikolaos Ilieskou <nikoloas.ilieskou@signify.com>
2021-03-03 11:24:39 +0100

    test for adding an item was added

--- item/item.service_test.go
+++ item/item.service_test.go
@@ -1,6 +1,7 @@
 package item_test
 
 import (
+	"bytes"
 	"database/sql"
 	"encoding/json"
 	"errors"
@@ -21,11 +22,17 @@ type ItemServiceTestSuite struct {
 	router *web.Router
 }
 
+const(
+	NEW_ITEM_ID = 3
+	MOCK_ERROR_STATUS = "error"
+	MOCK_NORMAL_STATUS = "normal"
+)
+
 func (suite *ItemServiceTestSuite) SetupTest() {
     suite.repo = NewMockRepo( []model.Item {
 		{1, "Levis", "Trousers", 33.0, 5,},
 		{2, "Nike", "Trousers", 34.0, 3,},
-	}, "normal")
+	}, MOCK_NORMAL_STATUS)
 	suite.service = model.NewService(suite.repo)
 	suite.router = item.InitRouter(suite.service)
 }
@@ -46,7 +53,7 @@ func (suite *ItemServiceTestSuite) TestGetAllSucceeds() {
 
 func (suite *ItemServiceTestSuite) TestGetAllFails() {
 	// Before 
-	suite.repo.status = "error"
+	suite.repo.status = MOCK_ERROR_STATUS
 	response, request := newTestRequest("GET", "/item")
 	
 	// Action
@@ -97,7 +104,7 @@ func (suite *ItemServiceTestSuite) TestGetByIDFailsNotFound() {
 
 func (suite *ItemServiceTestSuite) TestGetByIDFailsInternalServerError() {
 	// Before 
-	suite.repo.status = "error"
+	suite.repo.status = MOCK_ERROR_STATUS
 	response, request := newTestRequest("GET", "/item/1")
 	
 	// Action
@@ -107,7 +114,25 @@ func (suite *ItemServiceTestSuite) TestGetByIDFailsInternalServerError() {
 	suite.Equal(response.Result().StatusCode, http.StatusInternalServerError)
 }
 
+func (suite *ItemServiceTestSuite) TestAddSucceeds() {
+	// Before 
+	expected := fmt.Sprintf(`{"id":%v,"manufacturer":"Diesel","itemType":"Trousers","price":45,"quantity":6}`, NEW_ITEM_ID)
+	bodyRequest := `{"manufacturer":"Diesel","itemType":"Trousers","price":45,"quantity":6}`
+	request, _ := http.NewRequest("POST", "/item/", bytes.NewReader([]byte(bodyRequest)))
+	response := httptest.NewRecorder()
+	
+	// Action
+	suite.router.ServeHTTP(response, request)
+	
+	// Verify 
+	suite.Equal(response.Result().StatusCode, http.StatusCreated)
+	actual,err := suite.repo.GetByID(NEW_ITEM_ID)
+	suite.Equal(err, nil)
+	actualBytes,err := json.Marshal(actual)
+	suite.Equal(err, nil)
+	suite.Equal(actualBytes, []byte(expected))
 
+}
 
 func newTestRequest(method, path string) (*httptest.ResponseRecorder, *http.Request) {
 	request, _ := http.NewRequest(method, path, nil)
@@ -129,7 +154,7 @@ func NewMockRepo(items []model.Item, status string) *mockRepo{
 }
 
 func (m *mockRepo) GetByID(id int) (*model.Item, error) {
-	if m.status == "error" {
+	if m.status == MOCK_ERROR_STATUS {
 		return nil, errors.New("an error occured")
 	}
 	for _,i := range m.items {
@@ -141,14 +166,14 @@ func (m *mockRepo) GetByID(id int) (*model.Item, error) {
 }
 
 func (m *mockRepo) GetAll() ([]model.Item, error){
-	if m.status == "normal" {
+	if m.status == MOCK_NORMAL_STATUS {
 		return m.items, nil
 	} 
 	return nil, errors.New("an error occured")
 }
 
 func (m *mockRepo) Insert(newItem model.Item) error{
-	newItem.ID = 3
+	newItem.ID = NEW_ITEM_ID
 	m.items = append(m.items, newItem)
 	return nil
 }
